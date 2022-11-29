8e29e94ea4f0ca29be4f27180fb152fb8334ece3
LebedevaAlina-zp <lebedeva.alina.zp@gmail.ru>
2022-11-30 02:21:58 +0400

    Added requests from the STORE section

--- app/petstore_REST.py
+++ app/petstore_REST.py
@@ -2,11 +2,13 @@
 
 import requests
 import json
+from datetime import datetime
+
 
 base_url = 'https://petstore.swagger.io/v2'
 
 def print_response(response: requests.models.Response):
-    """Prints REST API responses of json or text type"""
+    """Prints the REST API response no matter it's json or text type"""
     print('Status code: ', response.status_code)
     print('Response:')
     if 'application/json' in response.headers['Content-Type']:
@@ -17,26 +19,26 @@ def print_response(response: requests.models.Response):
 
 # PET  Everything about your Pets
 THE_PET_API_SCENARIO = (
-    "Here's a scenario with REST API requests concerning actions with pets on the Petstore",
-    "Add a new pet with a POST /pet request, and save it's pet_id",
-    "Add a pet's image with POST /pet/{pet_id}/uploadImage",
-    "Update existing pet's info with PUT /pet request",
-    "Update existing pet's info with POST /pet/{pet_id} request",
-    "Check created pet with a GET /pet/{pet_id} request",
-    "Delete the pet with DELETE pet/{pet_id} request",
-    "Find all pets with status 'available', print first 5 of them, check there's no pet with pet_id"
+    "\nI. PET. \nHere are main REST API requests covering actions with pets on the Petstore according to https://petstore.swagger.io/",
+    "POST /pet - Add a new pet and save it's pet_id",
+    "POST /pet/{pet_id}/uploadImage - Add a pet's image",
+    "PUT /pet - Update existing pet's info",
+    "POST /pet/{pet_id} - Update existing pet's info",
+    "GET /pet/{pet_id} - Check created pet finding it by pet_id",
+    "DELETE pet/{pet_id} - Delete the pet",
+    "GET /pet/findByStatus - Find all pets with a status 'available', print first 5 of them, check there's no pet with pet_id"
     )
 
-j = 0 # The scenario list index, we'll add it by 1 when going to the next request
+j = 0
 print(THE_PET_API_SCENARIO[j])
 
 
 # POST /pet Add a new pet to the store
 j += 1
-print(f'\n{j}). {THE_PET_API_SCENARIO[j]}') # Add a new pet with a POST /pet request, and save it's pet_id
+print(f'\n{j}). {THE_PET_API_SCENARIO[j]}')
 
 body = {
-  "id": 0, # new id number will be assigned automatically
+  "id": 0,
   "category": {
     "id": 0,
     "name": "bird"
@@ -63,7 +65,7 @@ PET_id = res.json()['id']
 
 # POST /pet/{pet_id}/uploadImage Uploads an image
 j += 1
-print(f'\n{j}). {THE_PET_API_SCENARIO[j]}') # Add a pet's image with POST /pet/{pet_id}/uploadImage
+print(f'\n{j}). {THE_PET_API_SCENARIO[j]}')
 
 picture = open('Grusha.jpg', 'rb')
 
@@ -76,8 +78,7 @@ print_response(res)
 
 # PUT /pet Update an existing pet
 j += 1
-print(f'\n{j}). {THE_PET_API_SCENARIO[j]}') # Update pet's with pet_id info with PUT /pet request
-
+print(f'\n{j}). {THE_PET_API_SCENARIO[j]}')
 body = {
   "id": PET_id,
   "category": {
@@ -105,7 +106,7 @@ print_response(res)
 
 # POST /pet/{pet_id} Updates a pet in the store with form data
 j += 1
-print(f'\n{j}). {THE_PET_API_SCENARIO[j]}') # Update existing pet's info with POST /pet/{pet_id} request
+print(f'\n{j}). {THE_PET_API_SCENARIO[j]}')
 
 name_upd = 'Sara'
 status_upd = 'available'
@@ -118,7 +119,7 @@ print_response(res)
 
 # GET /pet/{pet_id} Find pet by ID
 j += 1
-print(f'\n{j}). {THE_PET_API_SCENARIO[j]}') # Check created pet with a GET /pet/{pet_id} request
+print(f'\n{j}). {THE_PET_API_SCENARIO[j]}')
 
 res = requests.get(f'{base_url}/pet/{PET_id}',
                    headers={'accept': 'application/json', 'Content-Type': 'multipart/form-data'})
@@ -136,7 +137,7 @@ print_response(res_del)
 
 # GET /pet/findByStatus Finds all the pets with certain status among: 'available', 'pending', 'sold'
 j += 1
-print(f'\n{j}). {THE_PET_API_SCENARIO[j]}') # Find all pets with status 'available', print first 5 of them
+print(f'\n{j}). {THE_PET_API_SCENARIO[j]}')
 
 res = requests.get(f'{base_url}/pet/findByStatus',
                    params={'status': 'available'},
@@ -156,3 +157,61 @@ for i in range(len(res.json())):
     elif i == len(res.json()) - 1:
         print(f'No item was found with the ID {PET_id}')
 
+
+########################################################################################################################
+
+# STORE  Access to Petstore orders
+THE_STORE_API_SCENARIO = (
+    "\n\nII. STORE. \nHere are main REST API requests related to access to the Petstore orders according to https://petstore.swagger.io/",
+    "POST /store/order - Place a new order, save its ID",
+    "GET /store/order/{orderId} - Find the order just created",
+    "DELETE /store/order/{orderId} - Delete the order by ID",
+    "GET /store/inventory - Check pet inventories by status"
+    )
+
+j = 0
+print(THE_STORE_API_SCENARIO[j])
+
+
+# POST /store/order Place an order for a pet
+j += 1
+print(f'\n{j}). {THE_STORE_API_SCENARIO[j]}')
+
+body = {
+  "id": 0,
+  "petId": 0,
+  "quantity": 0,
+  "shipDate": datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ"), # Make data and time format meet the swagger documentation
+  "status": "placed",
+  "complete": True
+}
+
+res = requests.post(f'{base_url}/store/order',
+                    headers={'accept': 'application/json', 'Content-Type': 'application/json'},
+                    data=json.dumps(body))
+print_response(res)
+order_id = res.json()['id']
+
+
+# GET /store/order/{orderId} Find purchase order by ID
+j += 1
+print(f'\n{j}). {THE_STORE_API_SCENARIO[j]}')
+
+res = requests.get(f'{base_url}/store/order/{order_id}', headers={'accept': 'application/json'})
+print_response(res)
+
+
+#DELETE /store/order/{orderId} - Delete purchase order by ID
+j += 1
+print(f'\n{j}). {THE_STORE_API_SCENARIO[j]}')
+
+res = requests.delete(f'{base_url}/store/order/{order_id}', headers={'accept': 'application/json'})
+print_response(res)
+
+
+# GET /store/inventory - Returns pet inventories by status
+j += 1
+print(f'\n{j}). {THE_STORE_API_SCENARIO[j]}')
+
+res = requests.get(f'{base_url}/store/inventory', headers={'accept': 'application/json', 'api_key': 'special-key'})
+print_response(res)
